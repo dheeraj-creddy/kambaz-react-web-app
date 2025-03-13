253e5962e6296724fb11d64fbd189565aa13c107
Dheeraj <dheerajreddy.in@gmail.com>
2025-03-12 23:52:13 -0400

    minor modifications part 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,17 @@ import store from "./Kambaz/store";
 import { Provider } from "react-redux";
 
 export default function App() {
-    return (
-        <HashRouter>
-            <Provider store={store}>
-                <div>
-                    <Routes>
-                        <Route path="/" element={<Navigate to="/Kambaz" />} />
-                        <Route path="/Labs/*" element={<Labs />} />
-                        <Route path="/Kambaz/*" element={<Kambaz />} />
-                    </Routes>
-                </div>
-            </Provider>
-        </HashRouter>
-    );
+  return (
+      <HashRouter>
+        <Provider store={store}>
+          <div>
+            <Routes>
+              <Route path="/" element={<Navigate to="/Kambaz" />} />
+              <Route path="/Labs/*" element={<Labs />} />
+              <Route path="/Kambaz/*" element={<Kambaz />} />
+            </Routes>
+          </div>
+        </Provider>
+      </HashRouter>
+  );
 }
