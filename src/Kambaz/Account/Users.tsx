3467217e77b29f0026ff43b6f3b3a1455743863c
Dheeraj <dheerajreddy.in@gmail.com>
2025-04-23 17:49:11 -0400

    update101 v2

--- src/Kambaz/Account/Users.tsx
+++ src/Kambaz/Account/Users.tsx
@@ -16,7 +16,7 @@ export default function Users() {
       fetchUsers();
     }
   };
-  const [name, setName] = useState("");
+  const [, setName] = useState("");
   const filterUsersByName = async (name: string) => {
     setName(name);
     if (name) {
